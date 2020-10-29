97c56e4e3bf52eb7820457fafe85c0eca71f1066
kyhe2 <69030251+kyhe2@users.noreply.github.com>
2020-10-29 13:57:05 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get install -y apache2
+sudo apt-get update -y
+sudo apt-get install -y apache2
 
-echo "Listen 8080" >> /etc/apache2/ports.conf
-service apache2 restart
+sudo echo "Listen 8080" >> /etc/apache2/ports.conf
+sudo service apache2 restart
 
-echo "<html>" > /var/www/html/index.html
-echo "<h1>Server2</h1>" >> /var/www/html/index.html
-echo "</html>" >> /var/www/html/index.html
+sudo echo "<html>" > /var/www/html/index.html
+sudo echo "<h1>Server2</h1>" >> /var/www/html/index.html
+sudo echo "</html>" >> /var/www/html/index.html
