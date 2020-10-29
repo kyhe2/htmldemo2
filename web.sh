b4dba566a27dab45c63796bb25e91634753dac63
kyhe2 <69030251+kyhe2@users.noreply.github.com>
2020-10-29 14:15:58 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -7,5 +7,5 @@ sudo echo "Listen 8080" >> /etc/apache2/ports.conf
 sudo service apache2 restart
 
 sudo echo "<html>" > /var/www/html/index.html
-sudo echo "<h1>Server2</h1>" >> /var/www/html/index.html
+sudo echo "<h1>Server2 Hello~~~~~~~~~~~!!!!</h1>" >> /var/www/html/index.html
 sudo echo "</html>" >> /var/www/html/index.html
