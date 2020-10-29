4ca0cd5d8e176a6814815584d7673eecac0aff17
kyhe2 <69030251+kyhe2@users.noreply.github.com>
2020-10-29 13:48:37 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -8,6 +8,4 @@ service apache2 restart
 
 echo "<html>" > /var/www/html/index.html
 echo "<h1>Server2</h1>" >> /var/www/html/index.html
-#echo "<img src=\"https://user03storage.blob.core.windows.net/cdnimages/2020090102.jpg\">" >> /var/www/html/index.html
-#echo "<img src=\"https://user03cndimagedemo.azureedge.net/cdnimages/2020090102.jpg\">" >> /var/www/html/index.html
 echo "</html>" >> /var/www/html/index.html
