954e1ea735afa60a32ebc6be6ad27d8ad4598816
kyhe2 <69030251+kyhe2@users.noreply.github.com>
2020-10-30 13:22:48 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y apache2
 sudo echo "Listen 8080" >> /etc/apache2/ports.conf
 sudo service apache2 restart
 
-sudo echo "<html>" > /var/www/html/index.html
-sudo echo "<h1>Server2 Hello~~~~~~~~~~~!!!!</h1>" >> /var/www/html/index.html
-sudo echo "</html>" >> /var/www/html/index.html
+echo "<html>" > /var/www/html/index.html
+echo "<h1>Server2</h1>" >> /var/www/html/index.html
+echo "<img src=\"https://www.city.kr/files/attach/images/164/943/875/011/e1e97d5f0c06cfb58e466a21ee8f9d2b.png\">" >> /var/www/html/index.html
+echo "</html>" >> /var/www/html/index.html
