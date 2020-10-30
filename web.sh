94190c55f794eedea5a48d87761b2c96cee83fe2
kyhe2 <69030251+kyhe2@users.noreply.github.com>
2020-10-30 14:21:48 +0900

    Update web.sh

--- web.sh
+++ web.sh
@@ -8,5 +8,5 @@ sudo service apache2 restart
 
 echo "<html>" > /var/www/html/index.html
 echo "<h1>Server2</h1>" >> /var/www/html/index.html
-echo "<img src=\"https://t1.daumcdn.net/cfile/blog/1927DD0B4B93B1AE11\">" >> /var/www/html/index.html
+echo "<img src=\"https://i.pinimg.com/originals/52/b3/9c/52b39cd0e5b9499fc2e0f3ec7184202f.jpg\">" >> /var/www/html/index.html
 echo "</html>" >> /var/www/html/index.html
